6ea10fa344dff886b9d87a2216dcd72bc6ebddbb
Stoner19 <Stoner19@users.noreply.github.com>
2017-11-28 21:26:22 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -63,7 +63,7 @@ let markets = [
     //
     // },
     {
-        marketName: 'bittrex',
+        marketName: 'bittrex-BTC',
         URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
         toBTCURL: false,
         pairURL : '',
@@ -91,68 +91,65 @@ let markets = [
         },
 
     },
-
     {
-        marketName: 'btc38',
-        URL: 'http://api.btc38.com/v1/ticker.php?c=all&mk_type=cny',
+        marketName: 'bittrex-ETH',
+        URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
         toBTCURL: false,
         pairURL : '',
-        last: function (data, coin_prices, toBTCURL) { //Where to find the last price of coin in JSON data
+        last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
-                let priceOfBTC = data.btc.ticker.last;
                 try {
-                    for (let key in data) {
-                        let coinName = key.toUpperCase();
-                        let price = data[key]['ticker'].last;
-                        if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                        coin_prices[coinName]['btc38'] = {
-                            bid: data[key]['ticker'].buy / priceOfBTC,
-                            ask: data[key]['ticker'].sell / priceOfBTC
-                        }; 
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('ETH-')) {
+                            let coinName = obj["MarketName"].replace("ETH-", '');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName]['bittrex'] = {
+                                bid: obj.Bid,
+                                ask: obj.Ask
+                            }; 
+                        }
                     }
                     res(coin_prices);
                 }
-
                 catch (err) {
                     console.log(err);
-                    rej(err)
+                    rej(err);
                 }
-            })
-        }
-    },
 
+            })
+        },
+
+    },
     {
-        marketName: 'jubi',
-        URL: 'https://www.jubi.com/api/v1/allticker/', //URL To Fetch API From.
-        toBTCURL: false, //URL, if needed for an external bitcoin price api.
+        marketName: 'bittrex-USDT',
+        URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
+        toBTCURL: false,
         pairURL : '',
-        last: function (data, coin_prices, toBTCURL) { //Where to find the last price of coin in JSON data
+        last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
-                let priceOfBTC = data.btc.last;
-                console.log(priceOfBTC);
                 try {
-                    for (let key in data) {
-                        let coinName = key.toUpperCase();
-                        let price = data[key].last;
-                        if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                        coin_prices[coinName]['jubi'] = {
-                            bid: data[key].buy / priceOfBTC,
-                            ask: data[key].sell / priceOfBTC
-                        }; 
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('USDT-')) {
+                            let coinName = obj["MarketName"].replace("USDT-", '');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName]['bittrex'] = {
+                                bid: obj.Bid,
+                                ask: obj.Ask
+                            }; 
+                        }
                     }
                     res(coin_prices);
                 }
-
                 catch (err) {
                     console.log(err);
-                    rej(err)
+                    rej(err);
                 }
+
             })
-        }
+        },
 
     },
-
-
+ 
     {
         marketName: 'poloniex',
         URL: 'https://poloniex.com/public?command=returnTicker',
